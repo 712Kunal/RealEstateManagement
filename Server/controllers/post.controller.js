6a3be1c19eb95db9af3bde7833add61e4a090d4c
712Kunal <jadhavkun0@gmail.com>
2024-09-30 20:54:46 +0530

    set the post controllers

---
+++ Server/controllers/post.controller.js
@@ -0,0 +1,17 @@
+const getAllPosts = async (req, res) => {};
+
+const getPostById = async (req, res) => {};
+
+const addPostById = async (req, res) => {};
+
+const updatePostById = async (req, res) => {};
+
+const deletePostById = async (req, res) => {};
+
+export {
+  getAllPosts,
+  getPostById,
+  addPostById,
+  updatePostById,
+  deletePostById,
+};
