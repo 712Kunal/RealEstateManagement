a29890aab12a712be53a1d3a788a42d19548d996
712Kunal <jadhavkun0@gmail.com>
2024-10-04 20:13:17 +0530

    added the react icons library

--- Client/src/Components/Signup.jsx
+++ Client/src/Components/Signup.jsx
@@ -6,8 +6,8 @@ function Signup() {
       <div className="flex justify-between items-center">
         <div className="ml-52 flex justify-center w-96 h-96">
           <form className="size-full max-w-[360px] flex flex-col items-center justify-center gap-5">
-            <h1 className="text-5xl font-Fredoka text-center text-white font-semibold mb-5">
-              Sign Up
+            <h1 className="text-4xl font-Fredoka text-center text-white font-semibold mb-5">
+              Create an account
             </h1>
             <input
               className="w-full bg-[#18181b] px-4 py-3 rounded-md border border-slate-700 outline-none text-slate-300 placeholder:text-gray-2 font-exo  tracking-widest text-base xl:text-lg font-medium"
@@ -25,7 +25,7 @@ function Signup() {
               placeholder="PASSWORD"
             />
             <button className="bg-[#5b21b6] font-sans tracking-wide text-2xl text-white p-3 px-16 rounded-lg hover:bg-[#4c1d95]">
-              Create An Account
+              Create Account
             </button>
             <p className="text-slate-200">
               Already have an account ? <a className="text-[#4f46e5]">Login</a>
