d6fdeeb814f487661b30f0f1ca065794e8b1a6ac
712Kunal <jadhavkun0@gmail.com>
2024-10-06 21:54:11 +0530

    added the client folder to the main repository

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import SignupPage from './Pages/Auth/SignupPage'
+
+function App() {
+
+  return (
+    <>
+      <SignupPage />
+    </>
+  )
+}
+
+export default App
