5d4d7c6d8620c8e00e91d177f41ead45ccfe2f96
712Kunal <jadhavkun0@gmail.com>
2024-10-08 23:12:32 +0530

    authSlice in the redux

--- Client/src/Features/Auth/AuthSlice.js
+++ Client/src/Features/Auth/AuthSlice.js
@@ -1,1 +1,34 @@
-import createSlice from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice(
+  {
+    name: "auth",
+    initialState: {
+      user: null,
+      isAuthenticated: false,
+      isVerified: false,
+    },
+  },
+
+  {
+    reducers: {
+      setUser: (state, action) => {
+        state.user = action.payload;
+        state.isAuthenticated = true;
+        state.isVerified = false;
+      },
+      setVerified: (state, action) => {
+        state.isVerified = true;
+      },
+      logout: (state, action) => {
+        state.user = null;
+        state.isAuthenticated = false;
+        state.isVerified = false;
+      },
+    },
+  }
+);
+
+export const { setUser, setVerified, logout } = authSlice.actions;
+
+export default authSlice.reducer;
