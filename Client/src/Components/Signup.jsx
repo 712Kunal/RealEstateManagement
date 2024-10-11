989a00e65b6613e4ccb4f72d72cb8945f3d0642e
712Kunal <jadhavkun0@gmail.com>
2024-10-11 23:10:23 +0530

    updated the UI of the signup page

--- Client/src/Components/Signup.jsx
+++ Client/src/Components/Signup.jsx
@@ -78,74 +78,70 @@ function Signup() {
 
   return (
     <div className="w-full h-screen overflow-hidden">
-      <div className="flex justify-evenly items-center">
-        <div className="bg-gray-900 p-4 rounded-xl shadow-2xl">
-          <form
-            onSubmit={handleSubmit}
-            className="flex flex-col items-center justify-center gap-6 w-full max-w-md"
-          >
-            <h1 className="text-5xl text-purple-500 font-Fredoka mb-2">
-              Create An Account
-            </h1>
-            <input
-              className="w-full text-gray-200 rounded-lg px-4 py-3 bg-gray-800 outline-none focus:ring-2 focus:ring-purple-500 transition duration-300 placeholder:tracking-widest text-xl font-medium"
-              type="text"
-              placeholder="USERNAME"
-              name="username"
-            />
-            <input
-              className="w-full text-gray-200 rounded-lg px-4 py-3 bg-gray-800 outline-none focus:ring-2 focus:ring-purple-500 transition duration-300 placeholder:tracking-widest text-xl font-medium"
-              type="email"
-              placeholder="EMAIL"
-              name="email"
-            />
-            <input
-              className="w-full text-gray-200 rounded-lg px-4 py-3 bg-gray-800 outline-none focus:ring-2 focus:ring-purple-500 transition duration-300 placeholder:tracking-widest text-xl font-medium"
-              type="password"
-              placeholder="PASSWORD"
-              name="password"
-            />
-            <Button
-              className="!w-full !text-2xl !rounded-lg !tracking-wider !p-3 !text-white !bg-purple-600 !hover:bg-purple-800 !transition !duration-300"
-              variant="contained"
-              type="submit"
-            >
-              Create Account
-            </Button>
-
-            <div className="w-full flex items-center my-2">
-              <div className="flex-grow h-px bg-gray-600"></div>
-              <span className="text-gray-400 text-md px-4">
-                OR CONTINUE WITH
-              </span>
-              <div className="flex-grow h-px bg-gray-600"></div>
-            </div>
-
-            <button className="w-full flex items-center justify-center gap-2 bg-white text-gray-800 font-semibold py-3 px-4 rounded-lg hover:bg-gray-100 transition duration-300">
-              <FcGoogle className="text-2xl" />
-              <span>Sign up with Google</span>
-            </button>
-
-            <p className="text-gray-400">
-              Already have an account?{" "}
-              <span
-                className="text-cyan-400 cursor-pointer hover:underline font-Fredoka transition duration-300"
-                onClick={() => Navigate("/login")}
-              >
-                Login
-              </span>
-            </p>
-          </form>
-        </div>
-
-        {/* IMAGE RENDERING */}
-        <div className="w-2/5 h-3/4">
-          <img
-            className="w-full h-full object-cover"
-            src="/signup.png"
-            alt="#SignUp_image"
+      <div className="bg-gray-900 p-4 rounded-xl shadow-2xl w-full max-w-md">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col items-center justify-center gap-6 w-full max-w-md"
+        >
+          <h1 className="text-5xl text-purple-500 font-Fredoka mb-2">
+            Create An Account
+          </h1>
+          <input
+            className="w-full text-gray-200 rounded-lg px-4 py-3 bg-gray-800 outline-none focus:ring-2 focus:ring-purple-500 transition duration-300 placeholder:tracking-widest text-xl font-medium"
+            type="text"
+            placeholder="USERNAME"
+            name="username"
           />
-        </div>
+          <input
+            className="w-full text-gray-200 rounded-lg px-4 py-3 bg-gray-800 outline-none focus:ring-2 focus:ring-purple-500 transition duration-300 placeholder:tracking-widest text-xl font-medium"
+            type="email"
+            placeholder="EMAIL"
+            name="email"
+          />
+          <input
+            className="w-full text-gray-200 rounded-lg px-4 py-3 bg-gray-800 outline-none focus:ring-2 focus:ring-purple-500 transition duration-300 placeholder:tracking-widest text-xl font-medium"
+            type="password"
+            placeholder="PASSWORD"
+            name="password"
+          />
+          <Button
+            className="!w-full !text-2xl !rounded-lg !tracking-wider !p-3 !text-white !bg-purple-600 !hover:bg-purple-800 !transition !duration-300"
+            variant="contained"
+            type="submit"
+          >
+            Create Account
+          </Button>
+
+          <div className="w-full flex items-center my-2">
+            <div className="flex-grow h-px bg-gray-600"></div>
+            <span className="text-gray-400 text-md px-4">OR CONTINUE WITH</span>
+            <div className="flex-grow h-px bg-gray-600"></div>
+          </div>
+
+          <button className="w-full flex items-center justify-center gap-2 bg-white text-gray-800 font-semibold py-3 px-4 rounded-lg hover:bg-gray-100 transition duration-300">
+            <FcGoogle className="text-2xl" />
+            <span>Sign up with Google</span>
+          </button>
+
+          <p className="text-gray-400">
+            Already have an account?{" "}
+            <span
+              className="text-cyan-400 cursor-pointer hover:underline font-Fredoka transition duration-300"
+              onClick={() => Navigate("/login")}
+            >
+              Login
+            </span>
+          </p>
+        </form>
+      </div>
+
+      {/* IMAGE RENDERING */}
+      <div className="w-2/5 h-3/4">
+        <img
+          className="w-full h-full object-cover"
+          src="src\assets\signup.png"
+          alt="#SignUp_image"
+        />
       </div>
 
       {is2faOpen ? (
--- Client/src/Pages/Auth/SignupPage.jsx
+++ Client/src/Pages/Auth/SignupPage.jsx
@@ -3,8 +3,27 @@ import Signup from '../../Components/Signup.jsx'
 
 function SignupPage() {
   return (
-    <div>
-      <Signup />
+    <div className="relative flex justify-center items-center min-h-screen w-full bg-gray-800 overflow-hidden">
+      {/* SVG Background */}
+      <div
+        className="absolute inset-y-0 left-0 right-0 z-0"
+        style={{
+          backgroundImage: "url('src/assets/SVG/signupwave.svg')",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      ></div>
+
+      {/* Content */}
+      <div className="relative z-10 flex grow md:flex-row justify-evenly items-center w-full max-w-6xl mx-auto p-5">
+        <Signup />
+        <img
+          src="src/assets/signup.png"
+          className="hidden md:block mt-0 w-1/2 h-1/2"
+          alt="signup page picture"
+        />
+      </div>
     </div>
   )
 }
