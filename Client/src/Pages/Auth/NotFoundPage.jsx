8eb6409dfd1075697e3cb901de539d2682500bec
712Kunal <jadhavkun0@gmail.com>
2024-10-18 00:59:57 +0530

    added the some UI to 404 page

--- Client/src/Pages/Auth/NotFoundPage.jsx
+++ Client/src/Pages/Auth/NotFoundPage.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function NotFoundPage() {
   return (
-    <div>NotFoundPage</div>
+    <div className='w-full h-screen overflow-hidden'>
+        <h1 className='font-bold font-exo text-9xl text-slate-200'>404</h1>
+
+        <div className="space-y-4">
+          <h2 className="text-4xl font-semibold text-slate-500">
+            Property Not Found
+          </h2>
+          <p className="text-slate-500 max-w-md mx-auto">
+            The property you're looking for seems to have moved to a different location or doesn't exist anymore.
+          </p>
+        </div>
+
+        <img src="src/assets/not Found.png" alt="imafd" />
+
+    </div>
   )
 }
 
