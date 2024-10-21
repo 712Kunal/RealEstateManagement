aed742390935de64ba1f6573177075f03e3dbfc2
712Kunal <jadhavkun0@gmail.com>
2024-10-21 22:30:40 +0530

    added the navitems

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav
-      className="w-full overflow-x-hidden fixed top-0 left-0 right-0 z-50 flex items-center justify-around gap-2"
+    <div
+      className="w-full overflow-x-hidden fixed top-0 left-0 right-0 z-50"
       style={{
         backgroundImage: "url(src/assets/SVG/navbarWave.svg)",
         backgroundRepeat: "no-repeat",
@@ -11,15 +11,28 @@ function Navbar() {
         backgroundPosition: "center",
       }}
     >
-      {/* LOGO */}
-      <div>
-        <img src="src/assets/logo.png" alt="logo" className="w-56" />
-      </div>
+      <nav className="flex flex-grow items-center justify-between mx-4">
+        {/* LOGO */}
+        <div className="flex items-center gap-20">
+          <img src="src/assets/logo.png" alt="logo" className="w-52 mt-1" />
 
-      {/* NAVBAR ITEMS */}
-      
+          {/* NAVBAR ITEMS */}
+          <div>
+            <ul className="flex items-center justify-around gap-16 text-white text-lg">
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+              <li>Agents</li>
+            </ul>
+          </div>
+        </div>
 
-    </nav>
+        <div className="buttons flex items-center justify-center gap-5">
+          <button className="bg-white text-black p-2">Login</button>
+          <button className="bg-black text-white p-2">Signup</button>
+        </div>
+      </nav>
+    </div>
   );
 }
 
