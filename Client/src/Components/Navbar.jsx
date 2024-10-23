3173b4084bbb8dc790559f6bb43bd6a09c5b4d53
712Kunal <jadhavkun0@gmail.com>
2024-10-24 00:43:54 +0530

    removed the menu items

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -57,19 +57,19 @@ function Navbar() {
           </button>
         </div>
 
-        <div className="menu-icon text-white cursor-pointer block md:hidden mr-4">
+        <div className="menu-icon block text-white cursor-pointer md:hidden mr-4 z-20">
           {" "}
           <IoMenu className="text-4xl" />
         </div>
 
-        <div className="menu text-white block md:hidden">
+        {/* <div className="menu flex flex-col items-center justify-center absolute right-0 top-0 bottom-0 h-full w-1/2 bg-black text-white md:hidden">
           <a href="">HOME</a>
           <a href="">ABOUT</a>
           <a href="">CONTACT</a>
           <a href="">AGENT</a>
           <a href="">SIGN UP</a>
           <a href="">LOGIN</a>
-        </div>
+        </div> */}
       </nav>
     </div>
   );
