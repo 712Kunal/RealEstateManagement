9e4c3efd24f96a55c1721f8e54de9541063458dc
712Kunal <jadhavkun0@gmail.com>
2024-10-24 00:09:12 +0530

    added the menu button

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { IoMenu } from "react-icons/io5";
 
 function Navbar() {
   return (
@@ -11,19 +12,21 @@ function Navbar() {
         backgroundPosition: "center",
       }}
     >
-      <nav className="flex flex-grow items-center justify-between mx-2">
+      <nav className=" w-full flex flex-grow items-center justify-between mx-2">
         {/* LOGO */}
-        <div className="nav-left flex items-center gap-12 basis-2/3 border-2 border-white">
+        <div className="nav-left flex items-center gap-12 basis-2/3">
           <a
             href="/"
             className="flex items-center gap-2 text-white hover:scale-105 transition-transform duration-300"
           >
             <img src="src/assets/logo.png" alt="logo" className="w-10" />
-            <span className="text-2xl font-courgette">REAL_EZY</span>
+            <span className="text-2xl md:hidden lg:block font-courgette">
+              REAL_EZY
+            </span>
           </a>
 
           {/* NAVBAR ITEMS */}
-          <div>
+          <div className="hidden md:block">
             <ul className="flex items-center justify-around gap-12 text-white text-lg">
               <li className="cursor-pointer hover:text-emerald-300 transition-colors duration-300 relative group">
                 Home
@@ -45,13 +48,16 @@ function Navbar() {
           </div>
         </div>
 
-        <div className="nav-right flex items-center justify-end h-full gap-5 basis-2/5 border-2 border-pink-500">
+        <div className="nav-right hidden md:flex items-center justify-end h-full gap-5 basis-2/5 border-2 border-pink-500">
           <button className="bg-gradient-to-r from-teal-400 to-emerald-600 text-white px-5 py-2 rounded-xl font-semibold hover:from-emerald-400 hover:to-teal-400 transition-all duration-300 transform hover:scale-105 hover:shadow-lg">
             Login
           </button>
           <button className="bg-gradient-to-r from-violet-400 to-purple-600 text-white px-5 py-2 rounded-xl font-semibold hover:from-purple-500 hover:to-violet-400 transition-all duration-300 transform hover:scale-105 hover:shadow-lg">
             Sign up
           </button>
+          <div className="menu-icon cursor-pointer">
+          <IoMenu />
+          </div>
         </div>
       </nav>
     </div>
