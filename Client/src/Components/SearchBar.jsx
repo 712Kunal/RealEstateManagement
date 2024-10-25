b5d2e098b3ea34e3bdd7d0d34840f332ad020556
712Kunal <jadhavkun0@gmail.com>
2024-10-26 01:17:27 +0530

    added some styling to the search bar

--- Client/src/Components/SearchBar.jsx
+++ Client/src/Components/SearchBar.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import { React, useState } from "react";
 
 function SearchBar() {
+  const types = ["Buy", "Rent"];
+
+  const [querry, setQuerry] = useState({
+    type: "Buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  //CHANGING THE TYPE OF THE BUTTONS
+  const switchType = (val) => {
+    setQuerry((prev) => ({ ...prev, type: val }));
+  };
+
   return (
-    <div>
-      <div className="type"></div>
+    <div className="flex flex-col items-start">
+      <div className="type">
+        {types.map((type) => {
+          return (
+            <button
+              key={type}
+              onClick={() => switchType(type)}
+              className={`py-4 px-9 border border-slate-200 cursor-pointer first:rounded-tl-lg first:rounded-tr-none last:rounded-tl-none last:rounded-tr-lg ${
+                querry.type === type ? "bg-slate-100" : ""
+              }`}
+            >
+              {type}
+            </button>
+          );
+        })}
+      </div>
       <form>
         <input type="text" name="location" placeholder="City Location" />
         <input
