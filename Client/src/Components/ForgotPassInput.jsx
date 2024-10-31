ac62380e77b5e5736207747f2cd92a74ed6635a9
712Kunal <jadhavkun0@gmail.com>
2024-10-31 20:48:27 +0530

    conditionally rendered loading state

--- Client/src/Components/ForgotPassInput.jsx
+++ Client/src/Components/ForgotPassInput.jsx
@@ -101,61 +101,63 @@ function ForgotPassInput({ open, onClose }) {
 
   return (
     <>
-      {loading ? <LoadingOverlay message={message} /> : null}
-
-      <Dialog
-        open={open}
-        onClose={onClose}
-        disableEscapeKeyDown
-        PaperProps={{
-          className:
-            "!bg-[#18181b] !text-white !shadow-md !shadow-cyan-500 !w-full",
-        }}
-      >
-        <div className="bg-[url('src/assets/SVG/popup.svg')] bg-cover bg-no-repeat bg-center">
-          <form onSubmit={handleSubmit}>
-            <DialogTitle className="!flex !items-center !gap-1 !font-old !text-4xl !font-normal">
-              <LockOpenIcon className="!text-4xl !text-center" />
-              Forgot Password
-            </DialogTitle>
-            <DialogContent>
-              <p className="!font-exo !tracking-wide !text-xl !font-extralight !mt-10">
-                Enter your email address and we'll send you a OTP to reset your
-                password.
-              </p>
-
-              <div className="flex flex-col w-full mt-10">
-                <p className="font-old text-xl font-extralight mb-1">
-                  Email Address :
+      {loading ? (
+        <LoadingOverlay message={message} />
+      ) : (
+        <Dialog
+          open={open}
+          onClose={onClose}
+          disableEscapeKeyDown
+          PaperProps={{
+            className:
+              "!bg-[#18181b] !text-white !shadow-md !shadow-cyan-500 !w-full",
+          }}
+        >
+          <div className="bg-[url('src/assets/SVG/popup.svg')] bg-cover bg-no-repeat bg-center">
+            <form onSubmit={handleSubmit}>
+              <DialogTitle className="!flex !items-center !gap-1 !font-old !text-4xl !font-normal">
+                <LockOpenIcon className="!text-4xl !text-center" />
+                Forgot Password
+              </DialogTitle>
+              <DialogContent>
+                <p className="!font-exo !tracking-wide !text-xl !font-extralight !mt-10">
+                  Enter your email address and we'll send you a OTP to reset
+                  your password.
                 </p>
-                <input
-                  className="w-full text-gray-100 rounded-lg px-2 py-1 bg-gray-800/50 border border-gray-700/50 outline-none focus:ring-2 focus:ring-violet-500/50 focus:border-violet-500/50 transition duration-300 placeholder:text-gray-400 placeholder:tracking-widest text-xl font-medium"
-                  type="email"
-                  placeholder="EMAIL"
-                  name="email"
-                  required
-                  pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
-                  title="Please enter a valid email address"
-                />
-              </div>
 
-              <Button
-                type="submit"
-                variant="contained"
-                className="!w-full !mt-5 tracking-wide !font-sans !text-xl text-white !p-2 rounded-lg"
-              >
-                NEXT
-              </Button>
+                <div className="flex flex-col w-full mt-10">
+                  <p className="font-old text-xl font-extralight mb-1">
+                    Email Address :
+                  </p>
+                  <input
+                    className="w-full text-gray-100 rounded-lg px-2 py-1 bg-gray-800/50 border border-gray-700/50 outline-none focus:ring-2 focus:ring-violet-500/50 focus:border-violet-500/50 transition duration-300 placeholder:text-gray-400 placeholder:tracking-widest text-xl font-medium"
+                    type="email"
+                    placeholder="EMAIL"
+                    name="email"
+                    required
+                    pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+                    title="Please enter a valid email address"
+                  />
+                </div>
 
-              {error && (
-                <p className="text-red-400 text-center bg-red-400/10 py-2 rounded-lg border border-red-400/20 mt-4">
-                  {error}
-                </p>
-              )}
-            </DialogContent>
-          </form>
-        </div>
-      </Dialog>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  className="!w-full !mt-5 tracking-wide !font-sans !text-xl text-white !p-2 rounded-lg"
+                >
+                  NEXT
+                </Button>
+
+                {error && (
+                  <p className="text-red-400 text-center bg-red-400/10 py-2 rounded-lg border border-red-400/20 mt-4">
+                    {error}
+                  </p>
+                )}
+              </DialogContent>
+            </form>
+          </div>
+        </Dialog>
+      )}
 
       {showMailVerify && (
         <div>
--- Client/src/Components/Forgotpassword.jsx
+++ Client/src/Components/Forgotpassword.jsx
@@ -60,75 +60,78 @@ function Forgotpassword({ open, onClose, email, onResetSuccess }) {
 
   return (
     <>
-      {loading ? <LoadingOverlay message={message} /> : null}
-      <Dialog
-        open={open}
-        onClose={onClose}
-        disableEscapeKeyDown
-        PaperProps={{
-          className:
-            "!bg-[#18181b] !text-white !shadow-md !shadow-cyan-500 !w-full",
-        }}
-      >
-        <div className="bg-[url('src/assets/SVG/popup.svg')] bg-cover bg-no-repeat bg-center">
-          <form onSubmit={handleSubmit}>
-            <DialogTitle className="!flex !items-center !gap-1 !font-old !text-4xl !font-normal">
-              <LockOpenIcon className="!text-4xl !text-center" />
-              Reset Account Password
-            </DialogTitle>
-            <DialogContent>
-              <div className="flex flex-col w-full mt-10">
-                <p className="font-exo text-xl font-extralight mb-1">
-                  New Password:
-                </p>
-                <input
-                  className="w-full text-gray-100 rounded-lg px-2 py-1 bg-gray-800/50 border border-gray-700/50 outline-none focus:ring-2 focus:ring-violet-500/50 focus:border-violet-500/50 transition duration-300 placeholder:text-gray-400 placeholder:tracking-widest text-xl font-medium"
-                  type="password"
-                  placeholder="Enter new password"
-                  name="new password"
-                  pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
-                  title="Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character (@$!%*?&)"
-                  required
+      {loading ? (
+        <LoadingOverlay message={message} />
+      ) : (
+        <Dialog
+          open={open}
+          onClose={onClose}
+          disableEscapeKeyDown
+          PaperProps={{
+            className:
+              "!bg-[#18181b] !text-white !shadow-md !shadow-cyan-500 !w-full",
+          }}
+        >
+          <div className="bg-[url('src/assets/SVG/popup.svg')] bg-cover bg-no-repeat bg-center">
+            <form onSubmit={handleSubmit}>
+              <DialogTitle className="!flex !items-center !gap-1 !font-old !text-4xl !font-normal">
+                <LockOpenIcon className="!text-4xl !text-center" />
+                Reset Account Password
+              </DialogTitle>
+              <DialogContent>
+                <div className="flex flex-col w-full mt-10">
+                  <p className="font-exo text-xl font-extralight mb-1">
+                    New Password:
+                  </p>
+                  <input
+                    className="w-full text-gray-100 rounded-lg px-2 py-1 bg-gray-800/50 border border-gray-700/50 outline-none focus:ring-2 focus:ring-violet-500/50 focus:border-violet-500/50 transition duration-300 placeholder:text-gray-400 placeholder:tracking-widest text-xl font-medium"
+                    type="password"
+                    placeholder="Enter new password"
+                    name="new password"
+                    pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+                    title="Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character (@$!%*?&)"
+                    required
+                    disabled={isSubmitting}
+                  />
+                </div>
+                <div className="flex flex-col w-full mt-5">
+                  <p className="font-exo text-xl font-extralight mb-1">
+                    Confirm Password:
+                  </p>
+                  <input
+                    className="w-full text-gray-100 rounded-lg px-2 py-1 bg-gray-800/50 border border-gray-700/50 outline-none focus:ring-2 focus:ring-violet-500/50 focus:border-violet-500/50 transition duration-300 placeholder:text-gray-400 placeholder:tracking-widest text-xl font-medium"
+                    type="password"
+                    placeholder="Confirm new password"
+                    name="confirm password"
+                    pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+                    title="Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character (@$!%*?&)"
+                    required
+                    disabled={isSubmitting}
+                  />
+                </div>
+                <Button
+                  type="submit"
+                  variant="contained"
                   disabled={isSubmitting}
-                />
-              </div>
-              <div className="flex flex-col w-full mt-5">
-                <p className="font-exo text-xl font-extralight mb-1">
-                  Confirm Password:
-                </p>
-                <input
-                  className="w-full text-gray-100 rounded-lg px-2 py-1 bg-gray-800/50 border border-gray-700/50 outline-none focus:ring-2 focus:ring-violet-500/50 focus:border-violet-500/50 transition duration-300 placeholder:text-gray-400 placeholder:tracking-widest text-xl font-medium"
-                  type="password"
-                  placeholder="Confirm new password"
-                  name="confirm password"
-                  pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
-                  title="Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character (@$!%*?&)"
-                  required
-                  disabled={isSubmitting}
-                />
-              </div>
-              <Button
-                type="submit"
-                variant="contained"
-                disabled={isSubmitting}
-                className="!w-full !mt-5 tracking-wide !font-sans !text-xl text-white !p-2 rounded-lg"
-              >
-                {isSubmitting ? "RESETTING PASSWORD..." : "RESET MY PASSWORD"}
-              </Button>
-              {error && (
-                <p className="text-red-400 text-center bg-red-400/10 py-2 rounded-lg border border-red-400/20 mt-4">
-                  {error}
-                </p>
-              )}
-              {success && (
-                <Alert severity="success" className="!mt-4">
-                  {success}
-                </Alert>
-              )}
-            </DialogContent>
-          </form>
-        </div>
-      </Dialog>
+                  className="!w-full !mt-5 tracking-wide !font-sans !text-xl text-white !p-2 rounded-lg"
+                >
+                  {isSubmitting ? "RESETTING PASSWORD..." : "RESET MY PASSWORD"}
+                </Button>
+                {error && (
+                  <p className="text-red-400 text-center bg-red-400/10 py-2 rounded-lg border border-red-400/20 mt-4">
+                    {error}
+                  </p>
+                )}
+                {success && (
+                  <Alert severity="success" className="!mt-4">
+                    {success}
+                  </Alert>
+                )}
+              </DialogContent>
+            </form>
+          </div>
+        </Dialog>
+      )}
     </>
   );
 }
