ee65b9fe5e35f4c8be3e8882820431c3d1e90f00
712Kunal <jadhavkun0@gmail.com>
2024-11-09 14:22:53 +0530

    added the local storage setup in the auth reducers

--- Client/src/Features/Auth/AuthSlice.js
+++ Client/src/Features/Auth/AuthSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { json } from "react-router-dom";
 
 const authSlice = createSlice({
   name: "auth",
@@ -12,14 +13,36 @@ const authSlice = createSlice({
       state.user = action.payload;
       state.isAuthenticated = true;
       state.isVerified = false;
+
+      // SAVES TO LOCAL STORAGE WHENEVER THE STATE CHANGES
+      localStorage.setItem(
+        "userData",
+        JSON.stringify({
+          user: action.payload,
+          isAuthenticated: true,
+          isVerified: false,
+        })
+      );
     },
     setVerified: (state, action) => {
       state.isVerified = true;
+
+      // SAVE THE UPDATED STATE TO LOCAL STORAGE
+      localStorage.setItem(
+        "userData",
+        JSON.stringify({
+          ...state,
+          isVerified: true,
+        })
+      );
     },
     logout: (state, action) => {
       state.user = null;
       state.isAuthenticated = false;
       state.isVerified = false;
+
+      // CLEARS THE LOCAL STORAGE
+      localStorage.removeItem("userData");
     },
   },
 });
