4198bb55247ac0ce06914108ccb9006b2726050a
712Kunal <jadhavkun0@gmail.com>
2024-11-10 16:51:32 +0530

    added the listpage in the routing

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,6 +6,7 @@ import HomePage from "./Pages/App/HomePage.jsx";
 import NotFoundPage from "./Pages/Auth/NotFoundPage.jsx";
 import LandingPage from "./Pages/Auth/LandingPage.jsx";
 import AppWrapper from "./Components/Wrappers/AppWrapper.jsx";
+import ListPage from "./Pages/App/ListPage.jsx";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
 
           <Route path="/app" element={<AppWrapper />}>
             <Route path="homepage" element={<HomePage />} />
+            <Route path="list" element={<ListPage />} />
           </Route>
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
