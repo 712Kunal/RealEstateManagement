1b23b229cf17ceb8d23d2e51e046812687641206
712Kunal <jadhavkun0@gmail.com>
2024-11-12 21:11:55 +0530

    redered map successsfully

--- Client/src/Components/Map.jsx
+++ Client/src/Components/Map.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { MapContainer } from "react-leaflet/MapContainer";
+import { TileLayer } from "react-leaflet/TileLayer";
+import { Marker } from "react-leaflet/Marker";
+import { Popup } from "react-leaflet/Popup";
 
 function Map() {
   const position = [51.505, -0.09];
 
-  render(
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+  return (
+    <MapContainer
+      center={position}
+      zoom={7}
+      scrollWheelZoom={false}
+      className="map-container w-full min-h-screen rounded-md overflow-hidden"
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- Client/src/Pages/App/ListPage.jsx
+++ Client/src/Pages/App/ListPage.jsx
@@ -3,17 +3,16 @@ import Filter from "../../Components/Filter";
 import Card from "../../Components/Card";
 import Map from "../../Components/Map";
 
-
 function ListPage() {
   return (
     <div className="ListPage min-h-screen flex relative inset-x-0 top-14 m-3">
       <div className="ListContainer basis-7/12">
-        <div className="wrapper flex flex-col gap-12">
+        <div className="wrapper flex flex-col gap-12 overflow-y-scroll pr-5">
           <Filter />
           <Card />
         </div>
       </div>
-      <div className="MapContainer basis-5/12">
+      <div className="MapContainer basis-5/12 h-full">
         <Map />
       </div>
     </div>
