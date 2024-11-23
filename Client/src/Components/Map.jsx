62e002884b61616bef7946567893955ae35db9f1
712Kunal <jadhavkun0@gmail.com>
2024-11-23 21:04:16 +0530

    updated map component with spinner

--- Client/src/Components/Map.jsx
+++ Client/src/Components/Map.jsx
@@ -2,45 +2,64 @@ import { React, useEffect, useState } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import Pin from "./Pin";
 import fetchTheData from "../lib/getPosts.js";
+import LoadingOverlay from "../Pages/Auth/LoadingOverlay.jsx";
 
 function Map() {
   const [error, setError] = useState("");
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    // CALLING THE FUNCTION TO FETCH THE POSTS FROM THE BACKEND SERVER
-    fetchTheData(setPosts, setError);
+    const fetchData = async () => {
+      setMessage("Fetching Data...");
+      setIsLoading(true);
+      // CALLING THE FUNCTION TO FETCH THE POSTS FROM THE BACKEND SERVER
+      await fetchTheData(setPosts, setError);
+      setIsLoading(false);
+    };
+
+    fetchData();
   }, []);
 
   const defaultPosition = [51.5074, -0.1278];
   console.log(posts);
 
   return (
-    <MapContainer
-      center={
-        posts.length === 1
-          ? [parseFloat(posts[0].latitude), parseFloat(posts[0].longitude)]
-          : defaultPosition
-      }
-      zoom={7}
-      scrollWheelZoom={false}
-      className="map-container w-full h-full rounded-md overflow-hidden outline-none"
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {posts.length > 0 ? (
-        posts.map((post) => (
-          <Pin
-            position={[parseFloat(post.latitude), parseFloat(post.longitude)]}
-            key={post.id || post._id}
-          />
-        ))
+    <>
+      {isLoading ? (
+        <LoadingOverlay message={message} />
       ) : (
-        <div className="text-center text-gray-500 py-8">Loading map...</div>
+        <MapContainer
+          center={
+            posts.length > 0
+              ? [parseFloat(posts[0].latitude), parseFloat(posts[0].longitude)]
+              : defaultPosition
+          }
+          zoom={7}
+          scrollWheelZoom={false}
+          className="map-container w-full h-full rounded-md overflow-hidden outline-none"
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {posts.length > 0 ? (
+            posts.map((post) => (
+              <Pin
+                position={[
+                  parseFloat(post.latitude),
+                  parseFloat(post.longitude),
+                ]}
+                key={post.id || post._id}
+              />
+            ))
+          ) : (
+            <div className="text-center text-gray-500 py-8">Loading map...</div>
+          )}
+        </MapContainer>
       )}
-    </MapContainer>
+    </>
   );
 }
 
