22b65e09b62317a6d6e8d2bca026e59282ad95c3
712Kunal <jadhavkun0@gmail.com>
2024-11-28 23:49:04 +0530

    added some styling to the image slider and single page

--- Client/src/Components/ImageSlider.jsx
+++ Client/src/Components/ImageSlider.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
 
 function ImageSlider() {
   return (
-    <div>ImageSlider</div>
-  )
+    <div className="Image Slider w-full flex gap-5 mb-5">
+      <div className="bigImage basis-4/5">
+        <img
+          src="/src/assets/hero.jpg"
+          alt="bigImage"
+          className="w-full h-full object-cover rounded-md cursor-pointer"
+        />
+      </div>
+      <div className="smallImages basis-1/5 flex flex-col justify-between gap-5">
+        <img
+          src="/src/assets/hero.jpg"
+          className="w-full h-[100px] object-cover rounded-md cursor-pointer"
+          alt=""
+        />
+        <img
+          src="/src/assets/hero.jpg"
+          className="w-full h-[100px] object-cover rounded-md cursor-pointer"
+          alt=""
+        />
+        <img
+          src="/src/assets/hero.jpg"
+          className="w-full h-[100px] object-cover rounded-md cursor-pointer"
+          alt=""
+        />
+        <img
+          src="/src/assets/hero.jpg"
+          className="w-full h-[100px] object-cover rounded-md cursor-pointer"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
 
-export default ImageSlider
+export default ImageSlider;
--- Client/src/Pages/App/SinglePage.jsx
+++ Client/src/Pages/App/SinglePage.jsx
@@ -5,28 +5,46 @@ import { MdLocationPin } from "react-icons/md";
 function SinglePage() {
   return (
     <div className="singlePage flex mt-16 mx-3">
-      <div className="details text-white text-4xl basis-8/12 border-2">
-        <div className="wrapper">
+      <div className="details text-gray-200 basis-8/12">
+        <div className="wrapper pr-12">
           <ImageSlider />
-          <div className="postInfo">
-            <div className="top info">
-              <div className="post">
-                <div className="address">
-                  <MdLocationPin />
+          <div className="postInfo flex flex-col gap-5">
+            <div className="top info flex justify-between">
+              <div className="post flex flex-col gap-2">
+                <h1 className="text-4xl font-old">Luxirious Flat</h1>
+                <div className="address flex items-center text-gray-400">
+                  <MdLocationPin className="text-lg" />
                   <span>Guwahati</span>
                 </div>
-                <div className="price">$ 1000</div>
+                <div className="price p-1 font-medium text-xl rounded-lg bg-blue-800 w-max">
+                  $ 1000
+                </div>
               </div>
-              <div className="user">
-                <img src="src/assets/noavatar.png" alt="user avatar" />
+              <div className="user flex flex-col items-center justify-center px-7 py-0 gap-2 rounded-lg font-semibold bg-blue-800">
+                <img
+                  src="/src/assets/noavatar.png"
+                  alt="user avatar"
+                  className="w-12 h-12 rounded-full object-cover"
+                />
+                <span>John Doe</span>
               </div>
             </div>
-            <div className="bottom info"></div>
+            <div className="bottom info mt-10 text-gray-400 leading-5 text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,
+              culpa nulla quas repudiandae iusto itaque incidunt iste tenetur
+              rerum perspiciatis maiores quibusdam nostrum excepturi ex rem.
+              Odit, quia perferendis. Consequatur sequi error ratione eum aut.
+              Laudantium cupiditate tempore omnis impedit dolorum ea veritatis.
+              Impedit quod cupiditate quia, corrupti tempora sunt!
+            </div>
           </div>
         </div>
       </div>
-      <div className="features basis-1/3 border-2">
-        <div className="wrapper"></div>
+
+      <div className="features basis-1/3">
+        <div className="h-full bg-[#164E63]/10 rounded-lg p-6">
+          {/* Features content will go here */}
+        </div>
       </div>
     </div>
   );
