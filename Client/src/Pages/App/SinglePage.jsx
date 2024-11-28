f0091af219e0d7e3c4254b90f55371fed5974105
712Kunal <jadhavkun0@gmail.com>
2024-11-28 21:26:41 +0530

    made image slider component

--- Client/src/Pages/App/SinglePage.jsx
+++ Client/src/Pages/App/SinglePage.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function SinglePage() {
   return (
-    <div className='singlePage flex mt-16 mx-3'>
-        <div className='details text-white text-4xl'>
-            jdskjs
+    <div className="singlePage flex mt-16 mx-3">
+      <div className="details text-white text-4xl basis-8/12 border-2">
+        <div className="wrapper">
+          
         </div>
-        <div className='features'>
-
-        </div>
+      </div>
+      <div className="features basis-1/3 border-2">
+        <div className="wrapper"></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SinglePage
+export default SinglePage;
