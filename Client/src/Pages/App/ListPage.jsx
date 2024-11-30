cb251b6915476d0db5923f53c93a0b57f1f75eb1
712Kunal <jadhavkun0@gmail.com>
2024-11-30 20:30:53 +0530

    again added the loading spinner to the list page

--- Client/src/Pages/App/ListPage.jsx
+++ Client/src/Pages/App/ListPage.jsx
@@ -4,14 +4,22 @@ import Card from "../../Components/Card";
 import fetchTheData from "../../lib/getPosts.js";
 import Map from "../../Components/Map";
 import { useLocation } from "react-router-dom";
+import LoadingOverlay from "../Auth/LoadingOverlay.jsx";
 
 function ListPage() {
   const [error, setError] = useState("");
   const [posts, setPosts] = useState([]);
+  const [message, setMessage] = useState("");
+  const [isloading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    // CALLING THE FUNCTION TO FETCH THE POSTS FROM THE BACKEND SERVER
-    fetchTheData(setPosts, setError);
+    const fetchData = async () => {
+      setMessage("Fetching Data...");
+      setIsLoading(true);
+      await fetchTheData(setPosts, setError);
+      setIsLoading(false);
+    };
+    fetchData();
   }, []);
 
   // GET THE LOCATION PARAM FROM THE URL
@@ -19,21 +27,31 @@ function ListPage() {
   const searchParams = new URLSearchParams(location.search);
   const locationParam = searchParams.get("location");
 
+  const handleCardClick = () => {
+    console.log("Clicked on card");
+  };
+
   return (
     <div className="ListPage flex mt-16 mx-3">
-      <div className="ListContainer basis-8/12">
-        <div className="wrapper flex flex-col gap-5 pr-5">
-          <Filter
-            place={locationParam}
-            setPosts={setPosts}
-            setError={setError}
-          />
-          <Card posts={posts} />
-        </div>
-      </div>
-      <div className="MapContainer hidden md:block basis-1/3 fixed top-16 right-3 h-[calc(100vh-4rem)] w-[calc(33.33%-12px)]">
-        <Map posts={posts} />
-      </div>
+      {isloading ? (
+        <LoadingOverlay message={message} />
+      ) : (
+        <>
+          <div className="ListContainer basis-8/12">
+            <div className="wrapper flex flex-col gap-5 pr-5">
+              <Filter
+                place={locationParam}
+                setPosts={setPosts}
+                setError={setError}
+              />
+              <Card posts={posts} onClick={handleCardClick} />
+            </div>
+          </div>
+          <div className="MapContainer hidden md:block basis-1/3 fixed top-16 right-3 h-[calc(100vh-4rem)] w-[calc(33.33%-12px)]">
+            <Map posts={posts} />
+          </div>
+        </>
+      )}
     </div>
   );
 }
